63c35df7f8e5fe8e9b95a27adc6891020f5ea1f3
qinzhihao-conghua <1043419697@qq.com>
2021-01-20 17:09:44 +0800

    openlayers点线面圆的绘制

--- src/app/ol-bd-map/ol-bd-map.component.ts
+++ src/app/ol-bd-map/ol-bd-map.component.ts
@@ -13,102 +13,102 @@ import projzh from 'projzh';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-ol-bd-map',
-    templateUrl: './ol-bd-map.component.html',
-    styleUrls: ['./ol-bd-map.component.scss']
+  selector: 'app-ol-bd-map',
+  templateUrl: './ol-bd-map.component.html',
+  styleUrls: ['./ol-bd-map.component.scss']
 })
 export class OlBdMapComponent implements OnInit, AfterViewInit {
 
-    constructor() { }
-    map: Map;
-    ngOnInit() {
+  constructor() { }
+  map: Map;
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    /**
+     *  定义百度投影，这是实现无偏移加载百度地图离线瓦片核心所在。
+     *  网上很多相关资料在用OpenLayers加载百度地图离线瓦片时都认为投影就是EPSG:3857(也就是Web墨卡托投影)。
+     *  事实上这是错误的，因此无法做到无偏移加载。
+     *  百度地图有自己独特的投影体系，必须在OpenLayers中自定义百度投影，才能实现无偏移加载。
+     *  百度投影实现的核心文件为bd09.js，在迈高图官网可以找到查看这个文件。
+     */
+    const projBD09 = new Projection({
+      // baidu或者BD:09
+      code: 'BD:09',
+      // 作用未知，不设置也可以
+      extent: [-20037726.37, -11708041.66, 20037726.37, 12474104.17],
+      units: 'm',
+      axisOrientation: 'neu',
+      global: false
+    });
+    addProjection(projBD09);
+    addCoordinateTransforms('EPSG:4326', projBD09, projzh.ll2bmerc, projzh.bmerc2ll);
+    addCoordinateTransforms('EPSG:3857', projBD09, projzh.smerc2bmerc, projzh.bmerc2smerc);
+
+    // 自定义分辨率和瓦片坐标系
+    const rlProject = [];
+
+    // 计算百度使用的分辨率
+    for (let i = 0; i <= 18; i++) {
+      rlProject[i] = Math.pow(2, 18 - i);
     }
+    const tilegrid = new TileGrid({
+      origin: [0, 0],    // 设置原点坐标
+      resolutions: rlProject // 设置分辨率
+    });
 
-    ngAfterViewInit(): void {
-        /**
-         *  定义百度投影，这是实现无偏移加载百度地图离线瓦片核心所在。
-         *  网上很多相关资料在用OpenLayers加载百度地图离线瓦片时都认为投影就是EPSG:3857(也就是Web墨卡托投影)。
-         *  事实上这是错误的，因此无法做到无偏移加载。
-         *  百度地图有自己独特的投影体系，必须在OpenLayers中自定义百度投影，才能实现无偏移加载。
-         *  百度投影实现的核心文件为bd09.js，在迈高图官网可以找到查看这个文件。
-         */
-        const projBD09 = new Projection({
-            // baidu或者BD:09
-            code: 'BD:09',
-            // 作用未知，不设置也可以
-            extent: [-20037726.37, -11708041.66, 20037726.37, 12474104.17],
-            units: 'm',
-            axisOrientation: 'neu',
-            global: false
-        });
-        addProjection(projBD09);
-        addCoordinateTransforms('EPSG:4326', projBD09, projzh.ll2bmerc, projzh.bmerc2ll);
-        addCoordinateTransforms('EPSG:3857', projBD09, projzh.smerc2bmerc, projzh.bmerc2smerc);
-
-        // 自定义分辨率和瓦片坐标系
-        const rlProject = [];
-
-        // 计算百度使用的分辨率
-        for (let i = 0; i <= 18; i++) {
-            rlProject[i] = Math.pow(2, 18 - i);
+    /**
+     * 加载百度地图离线瓦片不能用ol.source.XYZ，ol.source.XYZ针对谷歌地图（注意：是谷歌地图）而设计，
+     * 而百度地图与谷歌地图使用了不同的投影、分辨率和瓦片网格。
+     * 因此这里使用ol.source.TileImage来自行指定投影、分辨率、瓦片网格。
+     */
+    const baiduSource = new TileImage({
+      projection: 'BD:09',
+      tileGrid: tilegrid,
+      tilePixelRatio: 2,
+      tileUrlFunction: (tileCoord, pixelRatio, proj) => {
+        if (!tileCoord) {
+          return '';
         }
-        const tilegrid = new TileGrid({
-            origin: [0, 0],    // 设置原点坐标
-            resolutions: rlProject // 设置分辨率
-        });
+        const z = tileCoord[0];
+        let x: number | string = tileCoord[1];
+        // 注意，在openlayer3中由于载地图的方式是右上递增
+        // 而openlayer6中是右下递增，所以y的值需要注意
+        // 6版本需要取负值,同时注意要减一，否则缩放有问题
+        let y: number | string = -tileCoord[2] - 1;
 
-        /**
-         * 加载百度地图离线瓦片不能用ol.source.XYZ，ol.source.XYZ针对谷歌地图（注意：是谷歌地图）而设计，
-         * 而百度地图与谷歌地图使用了不同的投影、分辨率和瓦片网格。
-         * 因此这里使用ol.source.TileImage来自行指定投影、分辨率、瓦片网格。
-         */
-        const baiduSource = new TileImage({
-            projection: 'BD:09',
-            tileGrid: tilegrid,
-            tilePixelRatio: 2,
-            tileUrlFunction: (tileCoord, pixelRatio, proj) => {
-                if (!tileCoord) {
-                    return '';
-                }
-                const z = tileCoord[0];
-                let x: number | string = tileCoord[1];
-                // 注意，在openlayer3中由于载地图的方式是右上递增
-                // 而openlayer6中是右下递增，所以y的值需要注意
-                // 6版本需要取负值,同时注意要减一，否则缩放有问题
-                let y: number | string = -tileCoord[2] - 1;
+        // 百度瓦片服务url将负数使用M前缀来标识
+        if (x < 0) {
+          x = 'M' + (-x);
+        }
+        if (y < 0) {
+          y = 'M' + (-y);
+        }
+        // online3的3是用来分流的
+        // udt应该表示的是地图发布日期。p表示地图上的信息，scaler表示缩放
+        // 0表示不显示地名等标注信息，只单纯的地图地图，1表示显示地名信息
+        return `http://online3.map.bdimg.com/onlinelabel/?qt=tile&x=${x}&y=${y}&z=${z}&styles=pl&udt=20190426&scaler=2&p=1`;
+      }
+    });
 
-                // 百度瓦片服务url将负数使用M前缀来标识
-                if (x < 0) {
-                    x = 'M' + (-x);
-                }
-                if (y < 0) {
-                    y = 'M' + (-y);
-                }
-                // online3的3是用来分流的
-                // udt应该表示的是地图发布日期。p表示地图上的信息，scaler表示缩放
-                // 0表示不显示地名等标注信息，只单纯的地图地图，1表示显示地名信息
-                return `http://online3.map.bdimg.com/onlinelabel/?qt=tile&x=${x}&y=${y}&z=${z}&styles=pl&udt=20190426&scaler=2&p=1`;
-            }
-        });
-
-        // 创建地图
-        this.map = new Map({
-            layers: [
-                // 百度地图层
-                new TileLayer({
-                    source: baiduSource,
-                })
-            ],
-            view: new View({
-                center: transform([108.316492, 22.818136], 'EPSG:4326', 'BD:09'),
-                projection: 'BD:09',
-                zoom: 12,
-                maxZoom: 20,
-                minZoom: 3,
-                constrainResolution: true
-            }),
-            target: 'baiduMap2'
-        });
-    }
+    // 创建地图
+    this.map = new Map({
+      layers: [
+        // 百度地图层
+        new TileLayer({
+          source: baiduSource,
+        })
+      ],
+      view: new View({
+        center: transform([108.316492, 22.818136], 'EPSG:4326', 'BD:09'),
+        projection: 'BD:09',
+        zoom: 12,
+        maxZoom: 20,
+        minZoom: 3,
+        constrainResolution: true
+      }),
+      target: 'baiduMap2'
+    });
+  }
 
 }
--- src/app/ol-map/ol-map.component.ts
+++ src/app/ol-map/ol-map.component.ts
@@ -2,9 +2,13 @@ import { Map, View } from 'ol';
 import { defaults } from 'ol/control';
 import Attribution from 'ol/control/Attribution';
 import MousePosition from 'ol/control/MousePosition';
-import TileLayer from 'ol/layer/Tile';
 import { fromLonLat, toLonLat, transform } from 'ol/proj';
-import OSM from 'ol/source/OSM';
+import Point from 'ol/geom/Point';
+
+import { Circle as CircleStyle, Fill, Stroke, Style, Icon } from 'ol/style';
+import { Draw, Modify, Snap } from 'ol/interaction';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 
 import { AfterViewInit, Component, EventEmitter, OnInit, Output } from '@angular/core';
 
@@ -21,36 +25,129 @@ export class OlMapComponent implements OnInit, AfterViewInit {
   getCoordinate = new EventEmitter<number[]>();
   map: Map;
   coordinate: number[] = [];
+
+  // 栅格图层
+  raster = new TileLayer({
+    source: new OSM({
+      attributions: 'xxxx股份有限公司'
+    }),
+  });
+  // 矢量图层源
+  source = new VectorSource();
+  // 矢量图层
+  vector = new VectorLayer({
+    source: this.source,
+    style: new Style({
+      fill: new Fill({
+        color: 'rgba(255, 255, 255, 0.5)',
+      }),
+      stroke: new Stroke({
+        color: '#ff0000',
+        width: 2,
+      }),
+      image: new CircleStyle({ // 作用于点标注
+        radius: 7,
+        fill: new Fill({
+          color: '#0000ff',
+        }),
+      }),
+    }),
+  });
+
+
+  modify = new Modify({ source: this.source });
+  draw: Draw;
+  snap: Snap;
+  typeDraw = 'None';
+  mapCenter = fromLonLat([108.316492, 22.818136]);
+
   ngOnInit() {
   }
   ngAfterViewInit(): void {
-    const that = this;
     this.map = new Map({
       target: 'map',
       controls: defaults().extend([
         // 鼠标移入显示坐标
         new MousePosition({ projection: 'EPSG:4326' })
       ]),
-      layers: [
-        new TileLayer({
-          source: new OSM({
-            attributions: 'xxxx股份有限公司'
-          })
-        })
-      ],
+      layers: [this.raster, this.vector],
       view: new View({
-        center: fromLonLat([108.316492, 22.818136]),
+        center: this.mapCenter,
         zoom: 12,
         maxZoom: 20,
         minZoom: 6,
         projection: 'EPSG:3857'
       })
     });
-    this.map.on('singleclick', (event) => {
-      console.log(event.coordinate);
-      that.coordinate = transform(event.coordinate, 'EPSG:3857', 'EPSG:4326');
-      that.getCoordinate.emit(that.coordinate);
+    this.map.on('singleclick', this.mapSingleclick.bind(this));
+    // 可编辑图层
+    this.map.addInteraction(this.modify);
+    // this.addInteractions(this.typeDraw);
+  }
+  // 添加箭头线，失败
+  styleFunction = (feature) => {
+    const geometry = feature.getGeometry();
+    const styles = [
+      // linestring
+      new Style({
+        fill: new Fill({
+          color: 'rgba(255, 255, 255, 0.5)',
+        }),
+        stroke: new Stroke({
+          color: '#ff0000',
+          width: 2,
+        }),
+        image: new CircleStyle({ // 作用于点标注
+          radius: 7,
+          fill: new Fill({
+            color: '#0000ff',
+          }),
+        }),
+      })
+    ];
+
+    geometry.forEachSegment((start, end) => {
+      const dx = end[0] - start[0];
+      const dy = end[1] - start[1];
+      const rotation = Math.atan2(dy, dx);
+      // arrows
+      styles.push(
+        new Style({
+          geometry: new Point(end),
+          image: new Icon({
+            src: 'data/arrow.png',
+            anchor: [0.75, 0.5],
+            rotateWithView: true,
+            rotation: -rotation,
+          }),
+        })
+      );
     });
+
+    return styles;
+  }
+  // 地图单击
+  mapSingleclick(event) {
+    this.coordinate = transform(event.coordinate, 'EPSG:3857', 'EPSG:4326');
+    this.getCoordinate.emit(this.coordinate);
+  }
+  // 添加交互图层
+  addInteractions(typeDraw: any) {
+    if (typeDraw !== 'None') {
+      this.draw = new Draw({
+        source: this.source,
+        type: typeDraw,
+      });
+      this.map.addInteraction(this.draw);
+      this.snap = new Snap({ source: this.source });
+      this.map.addInteraction(this.snap);
+    }
+  }
+  // 交互类型
+  changeDrawing(type: any) {
+    this.map.removeInteraction(this.draw);
+    this.map.removeInteraction(this.snap);
+    this.addInteractions(type);
   }
 
 }
