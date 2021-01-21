7ea3f1ad4b8426950b23355ddbb17b8b03b92c0e
qinzhihao-conghua <1043419697@qq.com>
2021-01-21 15:45:14 +0800

    撒点以及水纹效果

--- src/app/ol-map/ol-map.component.ts
+++ src/app/ol-map/ol-map.component.ts
@@ -14,7 +14,10 @@ import { click, pointerMove, altKeyOnly } from 'ol/events/condition';
 
 import { AfterViewInit, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { SelectEvent } from 'ol/interaction/Select';
-import Feature from 'ol/render/Feature';
+import Feature from 'ol/Feature';
+import { easeOut } from 'ol/easing';
+import { getVectorContext } from 'ol/render';
+import { unByKey } from 'ol/Observable';
 
 @Component({
   selector: 'app-ol-map',
@@ -69,6 +72,11 @@ export class OlMapComponent implements OnInit, AfterViewInit {
   // 绘制类型
   typeDraw = 'None';
   mapCenter = fromLonLat([108.316492, 22.818136]);
+  points = [
+    [12058417.02420523, 2611140.7222976275],
+    [12059854.039403923, 2608327.839423466],
+    [12064501.410956929, 2611935.6663420903]
+  ];
 
   ngOnInit() {
   }
@@ -121,9 +129,9 @@ export class OlMapComponent implements OnInit, AfterViewInit {
       this.snap = new Snap({ source: this.source });
       this.map.addInteraction(this.snap);
       this.draw.on('drawend', (e) => {
-        // console.log('绘制结果', (e.feature.getGeometry() as any).getCoordinates());
+        console.log('绘制结果', (e.feature.getGeometry() as any).getCoordinates());
         // console.log('绘制结果', (e.feature.getGeometry() as any).getCenter());
-        console.log('绘制结果', e.feature);
+        // console.log('绘制结果', e.feature);
       });
       this.modify.on('modifyend', (e) => {
         console.log('修改结果', (e.features.item(0).getGeometry() as any).getCoordinates());
@@ -132,8 +140,7 @@ export class OlMapComponent implements OnInit, AfterViewInit {
   }
   // 选中图层，失去选中焦点也会触发
   changeInteraction(clickType: string, callback?: (e: SelectEvent) => void) {
-    this.map.removeInteraction(this.draw);
-    this.map.removeInteraction(this.snap);
+    this.clearInteraction();
     // 删除前需要先将修改功能取消，否则出现吸附在边缘时进行删除时报错
     this.map.removeInteraction(this.modify);
     if (this.select !== null) {
@@ -169,39 +176,111 @@ export class OlMapComponent implements OnInit, AfterViewInit {
       });
     }
   }
-  // selectLayer() {
-  //   this.changeInteraction('singleclick', (e) => {
-  //     console.log('选中动作', e);
-  //     if (e.selected.length > 0) {
-  //       this.vector.getSource().removeFeature(e.selected[0]);
-  //       this.map.removeInteraction(this.select);
-  //     }
-  //   });
-  // }
-  // 删除图层
+  // 选中并删除图层
+  selectLayer() {
+    this.changeInteraction('singleclick', (e) => {
+      console.log('选中动作', e);
+      if (e.selected.length > 0) {
+        this.vector.getSource().removeFeature(e.selected[0]);
+        // this.map.removeInteraction(this.select);
+        this.select.getFeatures().remove(e.selected[0]);
+      }
+    });
+  }
+  // 删除图层，未实现高亮选中后点击删除
   deleteLayer() {
     this.changeInteraction('pointermove', (e) => {
       const select = e;
-      this.map.on('singleclick', (single) => {
-        if (select.selected.length > 0) {
-          this.vector.getSource().removeFeature(select.selected[0]);
-          this.map.removeInteraction(this.select);
-          // this.map.addInteraction(this.modify);
-        }
+      this.select.on('click', () => {
+        this.select.getFeatures().forEach(item => {
+          this.vector.getSource().removeFeature(item);
+          this.select.getFeatures().remove(item);
+        });
       });
-      // 最好先解除select事件
-      // this.map.removeInteraction(this.select);
-      // 清空绘制的图层
-      // this.vector.getSource().clear();
     });
+  }
+  // 清空图层
+  clearLayer() {
+    // 最好先解除select事件
+    // this.map.removeInteraction(this.select);
+    // 清空绘制的图层
+    // this.vector.getSource().clear();
+    this.clearInteraction();
+    this.source.clear();
   }
 
   // 交互类型
   changeDrawing(type: any, rect?: string) {
+    this.clearInteraction();
+    this.addInteractions(type, rect);
+  }
+  // 清除交互
+  clearInteraction() {
     this.map.removeInteraction(this.draw);
     this.map.removeInteraction(this.snap);
     this.map.removeInteraction(this.select);
-    this.addInteractions(type, rect);
+  }
+
+  // 自定义动画
+  // https://openlayers.org/en/latest/examples/feature-animation.html
+  addPoint(points?: Array<any>) {
+    const features = [];
+    this.points.forEach(item => {
+      features.push(new Feature(new Point(item)));
+    });
+    this.source.on('addfeature', (e) => {
+      this.addAnimate(e.feature);
+    });
+    this.source.addFeatures(features);
+  }
+
+  addAnimate(feature) {
+    let start = new Date().getTime();
+    const duration = 3000;
+    const that = this;
+    // 进行地图水波渲染
+    this.raster.on('postrender', (event) => {
+      animate(event);
+    });
+    function animate(event) {
+      // 获取几何图形上下文
+      const vectorContext = getVectorContext(event);
+      // 获取当前渲染帧状态
+      const frameState = event.frameState;
+      // 复制OpenLayers.Geometry几何对象
+      const flashGeom = feature.getGeometry().clone();
+      // 渲染帧状已占时间
+      const elapsed = frameState.time - start;
+      // 已占比率
+      const elapsedRatio = elapsed / duration;
+      // radius半径为5结束为30
+      const dradius = easeOut(elapsedRatio) * 25 + 5;
+      // 不透明度渐变消失
+      const opacity = easeOut(1 - elapsedRatio);
+
+      const style = new Style({
+        image: new CircleStyle({
+          radius: dradius,
+          stroke: new Stroke({
+            color: 'rgba(255, 0, 0, ' + opacity + ')',
+            width: 0.25 + opacity,
+          }),
+          // fill: new Fill({
+          //   color: 'rgba(255, 0, 0, 0.5)',
+          // }),
+        }),
+      });
+      // 给几何图形添加样式
+      vectorContext.setStyle(style);
+      // 将几何体渲染到画布中
+      vectorContext.drawGeometry(flashGeom);
+      if (elapsed > duration) {
+        // 重复动画
+        start = frameState.time;
+      }
+      // tell OpenLayers to continue postrender animation
+      that.map.render();
+    }
   }
 
 }
